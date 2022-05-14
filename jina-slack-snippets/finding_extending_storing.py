b877d0f9f6939860c24728cb2b844009d9b93a4a
k-zehnder <kjzehnder3@gmail.com>
2022-05-14 16:29:49 -0700

    refactor: finding_extending_storing.py

--- jina-slack-snippets/finding_extending_storing.py
+++ jina-slack-snippets/finding_extending_storing.py
@@ -7,24 +7,51 @@ N, D = 100, 3
 documents = DocumentArray(
     storage='sqlite', 
     config={
-        'connection': '.sqlite_index'
+        'connection': 'jina-slack-snippets/persisted_data/sqlite_index'
     })
 
 with documents:
-    documents.extend([
-        Document(embedding=np.random.random(D), tags={'channel_id': str(i), 'idd' : str(i)}) for i in range(N)
-    ])
+    # documents.extend([
+    #     Document(embedding=np.random.random(D), tags={'channel_id': str(i)}) for i in range(N)
+    # ])
+    documents.extend([Document(embedding=np.random.random(D), tags={"channel_id" : str(i)}) for i in range(D)])
+# print(help(result.find))
 
-# result = documents.find(np.random.random(3))
-# result = documents.find({'modality': {'$eq': 'D'}})
+
+# find d in da by random embedding (nearest neighbor)
+result = documents.find(np.random.random(3))
+print(type(result)) # da
+print(result)
+result.summary()
+assert len(result) != 0
+assert len(result[0].scores) != 0 # checks to make sure first d in da has ".score" attribute now set as a "consequence" of being "found" by documents.find()
+
+result = documents.find(Document(embedding=np.array([0,0,0]), tags={"channel_id" : "0"}), limit=2, metric="cosine")
+print(type(result)) # NOTE: list
+print(result)
+result[0].summary()
+print(result[0].embeddings)
+
+# intentionally search for d that doesnt exist in D to understand this behavior
+result = documents.find({'modality': {'$eq': 'D'}})
+print(type(result)) # da
+assert len(result) == 0 # expecting not to find this d
+print(result)
+result.summary()
+
+# find d in da by specific tag number
 some_doc = Document(channel_id=str(10005))
 documents.extend([some_doc])
 result = documents.find({"tags__channel_id" : {"$eq" : "10005"}})
-assert len(result) > 0
-print(result)
-result = documents.find({"tags__channel_id" : {"$eq" : "42"}})
-assert len(result) > 0
-
+print(type(result))
+assert len(result) != 0
 print(result)
 result.summary()
 result[0].summary()
+
+# # same as above just selecting for equality for different value
+result = documents.find({"tags__channel_id" : {"$eq" : "42"}})
+assert len(result) == 0
+print(type(result))
+print(result)
+result.summary()
