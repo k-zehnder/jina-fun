3f963b871e35cfd708c9c7fdba148752d89470a8
k-zehnder <kjzehnder3@gmail.com>
2022-05-14 21:55:37 -0700

    add: show images notebook

---
+++ jina-slack-snippets/pdf_no_flow_fitz_text_only.py
@@ -0,0 +1,17 @@
+"""
+This script extracts TEXT only from PDF. It will not do anything with images.
+"""
+
+from docarray import Document, DocumentArray
+import fitz
+
+
+pdf = fitz.open('./data/pdf/sr71_medium.pdf')
+da = DocumentArray([Document(text=page.get_text().split("\n")) for page in pdf])
+
+da.summary()
+all_docs = da[:5, "text"]
+for doc in all_docs:
+    print(doc)
+    print("\n\n")
+print(da[:,'text'])
