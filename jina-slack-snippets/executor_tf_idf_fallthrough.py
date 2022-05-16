ed08e7fedcbab2772aceff56eadfa99a03cd99ef
Kevin Z <kjzehnder3@gmail.com>
2022-05-15 18:16:07 -0700

    refactor: executor tfidf practice

--- jina-slack-snippets/executor_tf_idf_fallthrough.py
+++ jina-slack-snippets/executor_tf_idf_fallthrough.py
@@ -6,23 +6,31 @@ class TFidf(Executor):
     def __init__(self, parameter, **kwargs):
         super().__init__(**kwargs)
         self.parameter = parameter
-        self.vocab = set()
     
+    @requests
+    # @requests(on="/prep")
+    # @requests(on="/index") # use this decorator when want to do f.index(inp)
     def prep_documents(self, docs, **kwargs):
+        vocab = set()
         for doc in docs:
-            if doc.text not in self.vocab:
-                self.vocab.add(doc.text)
-        return DocumentArray(vocab=self.vocab)
+            for word in doc.text.split(" "):
+                if word not in vocab:
+                    vocab.add(word)
+        voc = DocumentArray(Document(voc=list(vocab)))
+        # voc.summary()
+        return voc
 
 class Debugger(Executor):
     def __init__(self, parameter, **kwargs):
         super().__init__(**kwargs)
         self.parameter = parameter
 
-    @requests # NOTE: this "bare" requests is how we get the above Executor to "fallthrough" the flow to this executor b/c thats how the Flow specifies how out Documents should go through the Flow
+    @requests 
+    # NOTE: this "bare" requests is how we get the above Executor to "fallthrough" the flow to this executor b/c thats how the Flow specifies how out Documents should go through the Flow
     # if you didnt use a "bare" requests decorator, then this Executor wouldnt be visited by the Flow because it wouldn't know "where to go"
     def show_vocab(self, docs, **kwargs):
-        print(f"vocab: {docs}")
+        docs.summary()
+        print(docs[:, ("id", "tags__voc")])
 
 f = (
     Flow()
@@ -40,7 +48,9 @@ d3 = Document(text="I am an pilot from the sun doc3")
 inp = DocumentArray([d1, d2, d3])
 
 with f:
+    # r = f.post(on="/prep", inputs=inp)
     r = f.index(inp)
-
+    print(f"results: {r}")
+    
 print("[INFO] program complete.")
 r.summary()
